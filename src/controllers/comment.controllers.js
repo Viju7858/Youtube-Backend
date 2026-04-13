0f142aa13c25c9f0863166cb23302485bdb679e6
Viju7858 <vm72474@gmail.com>
2026-04-13 09:55:40 +0530

    add: implement comment controller functions for video comments

---
+++ src/controllers/comment.controllers.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add comment to video
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  //TODO: update comment
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  //TODO delete comment
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
